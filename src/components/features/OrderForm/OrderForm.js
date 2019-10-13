cfbba2dd6a71e4b9e078f5032188fde23bcf41f1
Przemek Szliep <szlipek@gmail.com>
2019-10-13 19:32:28 +0200

    send json

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,10 +5,51 @@ import pricing from '../../../data/pricing';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost, tripName, tripId, countryCode) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const name = options.name;
+  const contact = options.contact;
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripName,
+    tripId,
+    countryCode,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  if (name != '' && contact != '') {
+    fetch(url, fetchOptions)
+      .then(function (response) {
+        return response.json();
+      }).then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+      });
+  }
+
+
+};
 
 class OrderForm extends React.Component {
   render() {
-    const {tripCost, options, setOrderOption} = this.props;
+    const {tripCost, options, setOrderOption, tripName, tripId, countryCode} = this.props;
     return (
       <div>
         <Row>
@@ -19,6 +60,7 @@ class OrderForm extends React.Component {
           ))}
           <Col xs={12}>
             <OrderSummary cost={tripCost} options={options}/>
+            <Button onClick={() => sendOrder(options, tripCost, tripName, tripId, countryCode)}>Order now!</Button>
           </Col>
         </Row>
       </div>
@@ -30,6 +72,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.node,
   options: PropTypes.object,
   setOrderOption: PropTypes.node,
+  tripName: PropTypes.node,
+  countryCode: PropTypes.node,
+  tripId: PropTypes.string,
 };
 
 export default OrderForm;
--- src/components/features/OrderOption/OrderOptionText.js
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
 
-const OrderOptionText= () => (
-  <div className={styles.input}  >
-    <input type='text' />
+const OrderOptionText = ({setOptionValue}) => (
+  <div>
+    <input required className={styles.input} type='text' onChange={event => setOptionValue(event.currentTarget.value)}/>
   </div>
 );
+
+OrderOptionText.propTypes = {
+  setOptionValue: PropTypes.any,
+};
 
 export default OrderOptionText;
